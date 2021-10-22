8a0bfc52903e5a5c876537f408dee96ab703825b
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-22 11:55:19 -0300

    main methods

--- views/Main.java
+++ views/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
     public static void menu() {
         System.out.println(
-                "Menu\n1 - Cadastrar\n2 - Lista de Produto\n3 - Atualizar Produto\n4 - Remover Produto\n5 - sair\n");
+                "\nMenu\n1 - Cadastrar\n2 - Lista de Produto\n3 - Atualizar Produto\n4 - Remover Produto\n5 - sair\n");
     }
 
     public static int opcao() {
@@ -37,48 +37,74 @@ public class Main {
             break;
 
         case 2:
-
+            case2();
             break;
 
         case 3:
-
+            case3();
             break;
 
         case 4:
-
+            case4();
             break;
 
         case 5:
-               System.out.println("Obrigada!");
+            System.out.println("Obrigada!");
             break;
 
         default:
-            System.out.println("Não possui essa opção tente novamente ");
+            System.out.println("Não possui essa opção, tente novamente!");
             break;
-
         }
     }
 
     public static void case1() {
         Produto p = new Produto();
+        lerProduto(p);
+        pc.create(p);
+        System.out.println("\nProduto cadastrado com sucesso!");
+    }
 
-        System.out.println("Informe o id do produto:");
-        String id = sc.nextLine();
-        p.id = id;
+    public static void lerProduto(Produto p) {
+        p.id = lerString("Informe o id do produto: ");
+        p.nome = lerString("Infome o nome do produto: ");
+        p.categoria.descricao = lerString("Informe a categoria: ");
 
-        System.out.println("Infome o nome do produto:");
-        String produto = sc.nextLine();
-        p.nome = produto;
-
-        System.out.println("Informe a categoria:");
-        String categoria = sc.nextLine();
-        p.categoria.descricao = categoria;
-
-        System.out.println("Informe o preço:");
+        System.out.print("Informe o preço: ");
         double valor = Double.parseDouble(sc.nextLine());
         p.preco = valor;
+    }
 
-        pc.create(p);
+    public static String lerString(String mensagem) {
+        System.out.print(mensagem);
+        return sc.nextLine();
+    }
 
+    public static void case2() {
+        System.out.println("Lista de Produtos:");
+
+        if(pc.read().isEmpty()){
+            System.out.println("A lista está vazia!");
+        }else{
+            for (Produto pElement : pc.read()) {
+                System.out.println(pElement);
+            }
+        }
+    }
+
+    public static void case3() {
+        Produto p = new Produto();
+        lerProduto(p);
+        pc.update(p);
+        System.out.println("\nProduto atualizado com sucesso!");
+    }
+
+    public static void case4() {
+        Produto p = new Produto();
+
+        System.out.print("Informe o id do produto a ser removido: ");
+        p.id = sc.nextLine();
+        pc.delete(p);
+        System.out.println("\nProduto removido com sucesso");
     }
 }
