c1ccf4c68cfcc29e7f7b1a9a0f9b58c0360a84a8
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-22 11:55:10 -0300

    construtor Produto

--- models/Produto.java
+++ models/Produto.java
@@ -5,6 +5,10 @@ public class Produto extends BaseId{
     public double preco;
     public Categoria categoria;
 
+    public Produto(){
+        this.categoria = new Categoria();
+    }
+
     @Override
     public String toString() {
         return super.toString() + String.format("\nNome: %s \nPreço: %.2f", this.nome, this.preco) + this.categoria.toString();
