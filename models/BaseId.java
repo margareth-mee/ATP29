077ec954ea5bfce806dc2a78f30c59f3ac97fdef
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-22 10:30:46 -0300

    class BaseId

---
+++ models/BaseId.java
@@ -0,0 +1,15 @@
+package models;
+
+public class BaseId {
+    public String id;
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof BaseId) {
+            BaseId base = (BaseId) obj;
+            return this.id.equals(base.id);
+        }
+        return false;
+    }
+}
