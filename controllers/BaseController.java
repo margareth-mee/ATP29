e8fddcc97ddeadc189524a8b03ccac23e2dd5238
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-10-22 11:18:07 -0300

    Criando os controllers

--- controllers/BaseController.java
+++ controllers/BaseController.java
@@ -1,5 +1,37 @@
 package controllers;
 
-public class BaseController {
+import java.util.ArrayList;
+
+public class BaseController<B> {
+   private ArrayList<B> lista;
+
+   public BaseController(){
+       this.lista = new ArrayList<B>();
+   }
+
+   public void create(B obj){
+       this.lista.add(obj);
+   }
+
+   public ArrayList<B> read(){
+       return this.lista;
+   }
+
+   public void updade(B obj){
+     if(existe(obj)){
+         delete(obj);
+         create(obj);
+     }
+   }
+   
+   public void delete(B obj){
+       this.lista.remove(obj);
+   }
+
+   public boolean existe(B obj){
+       return this.lista.contains(obj);
+
+
+
     
 }
